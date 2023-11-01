e590d2a2feb3936f46e0f8868d187147e14d7f5d
David-Baca <david.baca.ingenieria@gmail.com>
2023-11-01 13:23:26 -0500

    Desarrollo de proyecto linea barrio

---
+++ LineaDeBarrio/src/main/java/Vistas/Principal.java
@@ -0,0 +1,40 @@
+package Vistas;
+
+public class Principal extends javax.swing.JFrame {
+    public Principal() {
+        initComponents();
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jButton1 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jButton1.setText("Iniciar sesion");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(157, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(145, 145, 145))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(233, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(44, 44, 44))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ LineaDeBarrio/src/main/java/upqroo/lineadebarrio/Main.java
@@ -0,0 +1,10 @@
+package upqroo.lineadebarrio;
+import Vistas.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+        Principal ventana = new Principal();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+    }
+}
